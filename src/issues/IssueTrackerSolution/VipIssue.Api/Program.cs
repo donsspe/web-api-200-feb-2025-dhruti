4a6913d056bf6f6566c2fef775968e748785e092
Dhruti Patel <donsspe@gmail.com>
2025-02-26 19:00:04 +0000

    check in vipissue module here

---
+++ src/issues/IssueTrackerSolution/VipIssue.Api/Program.cs
@@ -0,0 +1,51 @@
+using System.Formats.Tar;
+using System.Text.Json.Serialization;
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("postgres") ?? throw new Exception("Need a connection string");
+builder.Services.AddMarten(config =>
+{
+    config.Connection(connectionString);
+}).UseLightweightSessions();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapPost("/vips/notifications", async (VipIssueCreateModel request, IDocumentSession session) =>
+{
+    var response = new VipIssueResponseModel(
+        Guid.NewGuid(),
+        request.Problem,
+        request.Description,
+        "Pending");
+
+    session.Insert(response);
+    await session.SaveChangesAsync();
+    return TypedResults.Created($"/vip/notifications/{response.Id}", response);
+});
+
+app.MapGet("/vip/notifications/{id:guid}", async (Guid id, IDocumentSession session) => {
+
+    var response = await session.LoadAsync<VipIssueResponseModel>(id);
+
+    return TypedResults.Ok(response);
+});
+
+app.Run();
+
+public partial class Program { }
+
+public record VipIssueCreateModel(string Problem, string Description);
+
+public record VipIssueResponseModel(Guid Id, string Problem, string Description, string Status);
---
+++ src/issues/IssueTrackerSolution/VipIssue.Tests/NotifyingOfVipIssue.cs
@@ -0,0 +1,45 @@
+﻿
+using Alba;
+
+namespace VipIssue.Tests;
+
+public class NotifyingOfVipIssue
+{
+    [Fact]
+    public async Task CanAddANotification()
+    {
+        var host = await AlbaHost.For<Program>();
+        var requestBody = new VipIssueCreateModel("http://localhost:1339/problems/6bbe43c5-337a-4695-9875-a70985c8778a", "My stuff is broken again! Help!");
+        var postResponse = await host.Scenario(api =>
+        {
+            api
+            .Post
+            .Json(requestBody)
+            .ToUrl("/vips/notifications");
+
+            api.StatusCodeShouldBe(201);
+        });
+
+        var postResponseBody = postResponse.ReadAsJson<VipIssueResponseModel>();
+        var locationHeader = postResponse.Context.Response.Headers.Location.First();
+        Assert.NotNull(locationHeader);
+        Assert.StartsWith("/vip/notifications/", locationHeader);
+      
+
+        Assert.NotNull(postResponse);
+
+        Assert.Equal(requestBody.Problem, postResponseBody.Problem);
+        Assert.Equal(requestBody.Description, postResponseBody.Description);
+        Assert.Equal("Pending", postResponseBody.Status);
+        Assert.NotEqual(Guid.Empty, postResponseBody.Id);
+
+        var getResponse = await host.Scenario(api =>
+        {
+            api.Get.Url(locationHeader);
+        });
+
+        var getResponseBody = getResponse.ReadAsJson<VipIssueResponseModel>();
+
+        Assert.Equal(postResponseBody, getResponseBody);
+    }
+}
